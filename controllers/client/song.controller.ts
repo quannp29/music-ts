5bb81684baa3e9a32a2c18656ef455f2f7c9a976
Nguyen Phu Quan <93207485+npquan29@users.noreply.github.com>
2025-02-07 19:51:58 +0700

    display songs in topic

---
+++ controllers/client/song.controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import Topic from "../../models/topic.model";
+import Song from "../../models/song.model";
+import Singer from "../../models/singer.model";
+
+// [GET] /songs/:slugTopic
+export const list = async (req: Request, res: Response) => {
+  const slugTopic: string = req.params.slugTopic
+
+  const topic = await Topic.findOne({
+    slug: slugTopic,
+    deleted: false,
+    status: "active"
+  }).select("title");
+
+  const songs = await Song.find({
+    topicId: topic?.id,
+    deleted: false,
+    status: "active"
+  }).select("avatar title singerId like slug");
+
+  for (const item of songs) {
+    const singer = await Singer.findOne({
+      _id: item.singerId,
+      deleted: false
+    }).select("fullName");
+
+    item["singer"] = singer;
+  };
+
+  res.render("client/pages/songs/list", {
+    pageTitle: "Danh sách bài hát",
+    topic: topic,
+    songs: songs
+  });
+};

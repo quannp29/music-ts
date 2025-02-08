93a0aa59ad53881f0ec23907b3dc0a9b6d1f2f17
Nguyen Phu Quan <93207485+npquan29@users.noreply.github.com>
2025-02-08 15:37:43 +0700

    search advanced with slug

---
+++ controllers/client/search.controller.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import Song from "../../models/song.model";
+import Singer from "../../models/singer.model";
+import unidecode from "unidecode";
+
+// [GET] /search/result
+export const result = async (req: Request, res: Response) => {
+  const keyword: string = `${req.query.keyword}`;
+
+  // Loại bỏ dấu
+  const unidecodeText: string = unidecode(keyword);
+
+  // Loại bỏ khoảng trắng giữa các từ
+  const keywordSlug = unidecodeText.replace(/\s+/g, "-");
+
+  // Tạo Regex cho keywordSlug
+  const keywordSlugRegex = new RegExp(keywordSlug, "i");
+
+  const keywordRegex = new RegExp(keyword, "i");
+
+  let songs = [];
+  if (keyword) {
+    songs = await Song.find({
+      $or: [{ slug: keywordSlugRegex }, { title: keywordRegex }],
+      deleted: false,
+      status: "active",
+    }).select("avatar title singerId like slug");
+
+    for (const item of songs) {
+      const singer = await Singer.findOne({
+        _id: item.singerId,
+        deleted: false,
+      }).select("fullName");
+
+      item["singer"] = singer;
+    }
+  }
+
+  res.render("client/pages/search/result", {
+    pageTitle: `Kết quả: ${keyword}`,
+    keyword: keyword,
+    songs: songs,
+  });
+};

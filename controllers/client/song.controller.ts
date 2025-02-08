c23caddb6075578c8693bcf81e651578422725df
Nguyen Phu Quan <93207485+npquan29@users.noreply.github.com>
2025-02-08 10:00:36 +0700

    make favorite song

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import Topic from "../../models/topic.model";
 import Song from "../../models/song.model";
 import Singer from "../../models/singer.model";
+import FavoriteSong from "../../models/favorite-song.model";
 
 // [GET] /songs/:slugTopic
 export const list = async (req: Request, res: Response) => {
@@ -55,6 +56,12 @@ export const detail = async (req: Request, res: Response) => {
     deleted: false
   }).select("title");
 
+  const favoriteSong = await FavoriteSong.findOne({
+    userId: "",
+    songId: song.id
+  });
+
+  song["favorite"] = favoriteSong ? true : false;
 
   res.render("client/pages/songs/detail", {
     pageTitle: song.title,
@@ -96,4 +103,28 @@ export const like = async (req: Request, res: Response) => {
       message: "Không tồn tại bài hát",
     });
   }
+};
+
+// [PATCH] /songs/favorite/:status/:songId
+export const favorite = async (req: Request, res: Response) => {
+  const status = req.params.status;
+  const songId = req.params.songId;
+
+  if(status == "favorite") {
+    const favoriteSong = new FavoriteSong({
+      userId: "",
+      songId: songId
+    });
+    await favoriteSong.save();
+  } else {
+    await FavoriteSong.deleteOne({
+      userId: "",
+      songId: songId
+    });
+  }
+
+  res.json({
+    code: 200,
+    message: status == "favorite" ? "Đã thêm vào yêu thích" : "Đã xóa yêu thích"
+  });
 };
